34f3391661f50c65d782ead1237e31014b3a6a86
FelipeCruz <dacruzfe@bol.com.br>
2024-05-28 17:58:50 -0300

    Atualização dos exercicios do URI Online

--- Linguagem_CSharp_Basico/C#_Primeiros_Passos_Logica_Programacao_Algoritimos/Exercicios/Exercicios_uri/Uri1006/Uri1006/Program.cs
+++ Linguagem_CSharp_Basico/C#_Primeiros_Passos_Logica_Programacao_Algoritimos/Exercicios/Exercicios_uri/Uri1006/Uri1006/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Uri1006
 {
@@ -7,7 +8,16 @@ namespace Uri1006
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
+            double A, B, C, media;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (A * 2.0 + B * 3.0 + C * 5) / 10.0;
+
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
         }
     }
 }
