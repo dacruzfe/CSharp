a0f9517a7b4a379e939df6b8b271c2c0ec2bd95e
FelipeCruz <dacruzfe@bol.com.br>
2024-05-29 14:39:07 -0300

    resolução de novos exercicios do curso programação C#

---
+++ Linguagem_CSharp_Basico/C#_Primeiros_Passos_Logica_Programacao_Algoritimos/Exercicios/Exercicios_uri/Uri1009/Uri1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeVendedor = Console.ReadLine();
+
+            double salario, vendas, totalReceber;
+
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            totalReceber = salario + vendas * 0.15;
+
+            Console.WriteLine("TOTAL = R$ " + totalReceber.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Linguagem_CSharp_Basico/C#_Primeiros_Passos_Logica_Programacao_Algoritimos/Exercicios/Exercicios_uri/Uri1011/Uri1011/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double raio, volume;
+
+            //Recebo o valor do Raio
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Aqui uso os valores propostos no exercicio e faço o calculo para montar a esfera
+            volume = 4.0 / 3.0 * 3.14159 * raio * raio * raio;
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
