15fee726bae99ffb5ee19321de38d25384091a01
Felipe Cruz <dacruzfe@bol.com.br>
2025-03-30 14:24:32 -0300

    Atualização dos cursos e inclusão da branch feature-switch e arrumando o codigo pra ficar mais bonito inclusão do curso Desenvolvedor Junior C# - Básico

--- Linguagem_CSharp_Basico/programador-desenvolvedor junior em C#/CursoCSharpTesteCalculadoraConsoleApp/CursoCSharpTesteCalculadoraConsoleApp/Program.cs
+++ Linguagem_CSharp_Basico/programador-desenvolvedor junior em C#/CursoCSharpTesteCalculadoraConsoleApp/CursoCSharpTesteCalculadoraConsoleApp/Program.cs
@@ -1,51 +1,60 @@
 ﻿Console.WriteLine("Seja Bem Vindo a Calculadora!");
-//Receber 2 valores
-//Solicitar os dois valores para o usuário digitar
-Console.WriteLine("Digite o primeiro valor:");
-string? primeiroValorString = Console.ReadLine();
-double primeiroValor = Convert.ToDouble(primeiroValorString);
 
-
-Console.WriteLine("Digite o segundo valor:");
-string? segundoValorString = Console.ReadLine();
-double segundoValor = Convert.ToDouble(segundoValorString);
-
-//Dar opções de calculo
-Console.WriteLine("Selecione a operação:");
-Console.WriteLine("+");
-Console.WriteLine("-");
-Console.WriteLine("*");
-Console.WriteLine("/");
-
-var operacaoSelecionada = Console.ReadLine();
-
-//Realizar o calculo
-
-double resultadoOperacao;  
-switch (operacaoSelecionada)
+do
 {
-    case "+":
-        resultadoOperacao = primeiroValor + segundoValor;
-        Console.WriteLine($"Resultado da operacao: {resultadoOperacao}");
-        break;
-    case "-":
-        resultadoOperacao = primeiroValor - segundoValor;
-        Console.WriteLine($"Resultado da operacao: {resultadoOperacao}");
-        break;
-    case "*":
-        resultadoOperacao = primeiroValor * segundoValor;
-        Console.WriteLine($"Resultado da operacao: {resultadoOperacao}");
-        break;
-    case "/":
-        resultadoOperacao = primeiroValor / segundoValor;
-        Console.WriteLine($"Resultado da operacao: {resultadoOperacao}");
-        break;
-    default:
-        Console.WriteLine("Operação inválida!");
-        break;
-}
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor:");
+    string? primeiroValorString = Console.ReadLine();
+    double primeiroValor = Convert.ToDouble(primeiroValorString);
 
-//Exibir o resultado
-//Finalizar a aplicação
+
+    Console.WriteLine("Digite o segundo valor:");
+    string? segundoValorString = Console.ReadLine();
+    double segundoValor = Convert.ToDouble(segundoValorString);
+
+    Console.WriteLine("Selecione a operação:");
+    Console.WriteLine("+");
+    Console.WriteLine("-");
+    Console.WriteLine("*");
+    Console.WriteLine("/");
+
+    var operacaoSelecionada = Console.ReadLine();
+
+    double? resultadoOperacao;
+    switch (operacaoSelecionada)
+    {
+        case "+":
+            resultadoOperacao = primeiroValor + segundoValor;
+            break;
+        case "-":
+            resultadoOperacao = primeiroValor - segundoValor;
+            break;
+        case "*":
+            resultadoOperacao = primeiroValor * segundoValor;
+            break;
+        case "/":
+            resultadoOperacao = primeiroValor / segundoValor;
+            break;
+        default:
+            resultadoOperacao = null;
+            break;
+    }
+
+    //if (resultadoOperacao.HasValue)
+    //    Console.WriteLine($"Resultado da operacao: {resultadoOperacao}");
+    //else
+    //    Console.WriteLine("Operação inválida!");
+
+    Console.WriteLine();
+    var mensagem = resultadoOperacao.HasValue ? $"Resultado da operação: {resultadoOperacao}" : "Operação inválida!";
+    Console.WriteLine(mensagem);
+
+
+    Console.WriteLine();
+    Console.WriteLine("Deseja realizar outra operação?");
+    Console.WriteLine("(S) Sim (N) Não");
+} while (Console.ReadKey().Key == ConsoleKey.S);
+
+Console.WriteLine("Até logo!!");
 
 Console.ReadKey();
--- Linguagem_CSharp_Basico/programador-desenvolvedor junior em C#/CursoCSharpTesteCalculadoraConsoleApp/CursoCSharpTesteCalculadoraConsoleApp/obj/Debug/net8.0/CursoCSharpTesteCalculadoraConsoleApp.AssemblyInfo.cs
+++ Linguagem_CSharp_Basico/programador-desenvolvedor junior em C#/CursoCSharpTesteCalculadoraConsoleApp/CursoCSharpTesteCalculadoraConsoleApp/obj/Debug/net8.0/CursoCSharpTesteCalculadoraConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharpTesteCalculadoraConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19c8fbb5c71e7aee5aa8657444fb0856b6d85b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d1f8c97490172a1d571822995ffbc13feaa7c12")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharpTesteCalculadoraConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharpTesteCalculadoraConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
